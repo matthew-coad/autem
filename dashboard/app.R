29609b6e2e1e28e6e3950815b024d9117d4833b4
Matthew Coad <matthew_coad@hotmail.com>
2019-01-31 19:45:13 +1100

    Create R dashboard

---
+++ dashboard/app.R
@@ -0,0 +1,120 @@
+library(shinydashboard)
+
+source("simulations.R")
+
+simulations_path <- "D:\\Documents\\autem\\benchmark\\simulations"
+
+header <- function()
+  dashboardHeader(title = "Autem")
+
+sidebar <- function()
+  dashboardSidebar(
+    sidebarMenu(
+      selectInput("simulation", "Simulation", choices = "Loading..."),
+      selectInput("kpi", "KPI", choices = "Loading..."),
+      menuItem("Progress", tabName = "progress", icon = icon("dashboard")),
+      menuItem("Performance", tabName = "performance", icon = icon("dashboard"))
+    )
+  )
+
+progress_tab <- function()
+  tabItem(tabName = "progress",
+    fluidRow(
+        valueBoxOutput("status_steps")
+    ),
+          
+    fluidRow(
+      box(
+        title = "Events", status = "primary", solidHeader = TRUE, width = 6,
+        plotOutput("population_progress", height = 250)
+      ),
+      box(
+        title = "KPI", status = "primary", solidHeader = TRUE, width = 6,
+        plotOutput("progress_kpi", height = 250)
+      )
+    )
+  )
+
+performance_tab <- function()
+  tabItem(tabName = "performance",
+    fluidRow(
+      column(width = 4,
+        box(
+          title = "Perf Box title", width = NULL, status = "primary",
+          "Perf"
+        )
+      )
+    )
+  )
+
+body <- function() dashboardBody(tabItems(progress_tab(), performance_tab()))
+
+
+# We'll save it in a variable `ui` so that we can preview it in the console
+ui <- dashboardPage(
+  header(),
+  sidebar(),
+  body()
+)
+
+# Preview the UI in the console
+shinyApp(ui = ui, server = function(input, output, session) { 
+  
+  simulations <- reactive({
+    load_simulation_choices(simulations_path)
+  })
+  
+  observe({
+      updateSelectInput(session, "simulation", choices = simulations())
+  })
+  
+  outline_df <- reactive({
+    simulation <- input$simulation
+    if (simulation == "Loading...")
+      return(NULL)
+    outline_df <- load_outline_df(input$simulation)
+    outline_df
+  })
+  
+  observe({
+    outline_df_value <- outline_df()
+    if (is.null(outline_df_value))
+      return()
+    kpis_df <- outline_df_value %>% dplyr::filter(role == "kpi")
+    choices <- kpis_df$label %>% set_names(kpis_df$name)
+    updateSelectInput(session, "kpi", choices = choices)
+  })
+
+  battle_df <- reactive({
+    simulation <- input$simulation
+    if (simulation == "Loading...")
+      return(NULL)
+    battle_df <- load_battle_df(input$simulation)
+    battle_df
+  })
+  
+  
+  progress_df <- reactive({
+    battle_df_value <- battle_df()
+    if (is.null(battle_df_value))
+      return(NULL)
+    df <- evaluate_progress_df(battle_df_value)
+    df
+  })
+  
+  status <- reactive({
+    battle_df_value <- battle_df()
+    if (is.null(battle_df_value))
+      return(NULL)
+    result <- evaluate_status(battle_df_value)
+    result
+  })
+  
+  output$status_steps <- renderValueBox({
+    current_status <- status()
+    valueBox(paste0(current_status$step), "Steps", color = "purple", icon = icon("list"))
+  })
+  output$population_progress <- renderPlot({population_progress_plot(progress_df())})
+  output$progress_kpi <- renderPlot({progress_kpi_plot(progress_df())})
+
+})
---
+++ dashboard/simulations.R
@@ -0,0 +1,104 @@
+library(tidyverse)
+library(readr)
+
+load_simulations_df <- function(path) {
+  dirs <- list.dirs(path, full.names = FALSE) %>% setdiff("")
+  
+  load_simulation_dir <- function(dir) {
+    simulation <- tibble::tibble(
+      name = dir,
+      path = file.path(path, dir)
+    )
+  }
+  df <- purrr::map_dfr(dirs, load_simulation_dir)
+  df
+}
+
+load_simulation_choices <- function(path) {
+  df <- load_simulations_df(path)
+  choices <- df$path %>% set_names(df$name)
+  choices
+}
+
+load_outline_df <- function(path) {
+  outline_path <- file.path(path, "Outline.csv")
+  col_types <- cols(
+    name = col_character(),
+    dataset = col_character(),
+    role = col_character(),
+    label = col_character()
+  )
+  df <- suppressMessages(read_csv(outline_path, col_names = TRUE, col_types = col_types))
+  df
+}
+
+
+# Load battle data for a simulation
+load_battle_df <- function(path) {
+  
+  files <- list.files(path, "Battle*")
+  
+  load_battle_file <- function(filename) {
+    battle_path <- file.path(path, filename)
+    df <- suppressMessages(read_csv(battle_path))
+    df
+  }
+  
+  df <- purrr::map_dfr(files, load_battle_file)
+  df
+}
+
+# Load battle data for a simulation
+load_battle_df <- function(path) {
+  
+  files <- list.files(path, "Battle*")
+  
+  load_battle_file <- function(filename) {
+    battle_path <- file.path(path, filename)
+    df <- suppressMessages(read_csv(battle_path))
+    df
+  }
+  
+  df <- purrr::map_dfr(files, load_battle_file)
+  df
+}
+
+# Generate simulation status report from the battle data
+evaluate_status <- function(battle_df) {
+  steps <- max(battle_df$step)
+  list(step = steps)
+}
+
+# Generate progress report
+evaluate_progress_df <- function(battle_df) {
+  df <- battle_df %>%
+    mutate(
+      alive = cumsum(n_alive), 
+      mature = cumsum(n_mature), 
+      attractive = cumsum(n_attractive),
+      score = 0
+    ) %>% 
+    select(step, alive, mature, attractive, score)
+  df
+}
+
+population_progress_plot <- function(progress_df) {
+  plot <- progress_df %>%
+    ggplot(aes(x = step)) +
+    geom_line(aes(y = alive, color = "alive")) +
+    geom_line(aes(y = mature, color = "mature")) +
+    geom_line(aes(y = attractive, color = "attractive")) +
+    xlab("Step") +
+    ylab("Population")
+  plot
+}
+
+progress_kpi_plot <- function(progress_df) {
+  plot <- progress_df %>%
+    ggplot(aes(x = step)) +
+    geom_point(aes(y = score), na.rm = TRUE) +
+    xlab("Step") +
+    ylab("KPI")
+  plot
+}
+
