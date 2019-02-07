99a3a6ff268a273364e75de98d75e55e2f16c708
Matthew Coad <matthew_coad@hotmail.com>
2019-02-07 22:51:38 +1100

    Setup benchmark experiment

--- benchmark/benchmark_experiment.py
+++ benchmark/benchmark_experiment.py
@@ -2,11 +2,11 @@ if __name__ == '__main__':
     import context
 
 import benchmark.baseline_experiment as baseline
-import benchmark.select_experiment as select
+import benchmark.preprocess_experiment as preprocess
 
 def run_experiment():
     baseline.run_experiment()
-    select.run_experiment()
+    preprocess.run_experiment()
 
 if __name__ == '__main__':
     run_experiment()
